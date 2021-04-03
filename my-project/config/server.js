e381506d9560da78590f4ce8bb35f06dfbe8547b
Mahmoud Selim <mahmoudsaleam11@gmail.com>
2021-04-03 13:54:37 +0200

    strapimongodb

---
+++ my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'babe5c18cd2f8c72a6cc20794cc15339'),
+    },
+  },
+});
---
+++ my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '80e924c5-781b-402b-8b6c-6683f06f520e'
+};
